1809369165a19e83de4ef11a8851153f061d3e18
pathakcodes <shivampathak339@gmail.com>
2019-01-18 21:41:54 +0530

    updated repo

--- pathakCodes/1.py
+++ pathakCodes/1.py
@@ -8,7 +8,7 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
-'female', 'male', 'male']
+'female', 'male', 'femal']
 
 clf = tree.DecisionTreeClassifier()
 
